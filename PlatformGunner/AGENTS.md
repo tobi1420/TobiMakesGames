7221c8dccb976b930bb54e42391785df68261af2
tobiaslarsen1995 <tobiaslarsen1995@gmail.com>
2025-11-29 11:10:48 +0100

    Spawning the Player Object in a Random Color

---
+++ PlatformGunner/AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: PlatformGunner
+- Unity version: Unity 6000.2.14f1
+- Active game object:
+  - Name: Square
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- PlatformGunner/Assets/Player.cs
+++ PlatformGunner/Assets/Player.cs
@@ -1,25 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteColorChanger : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
+    
+    public List<Color> colors = new List<Color>()
+    {
+        Color.green,
+        Color.blue,
+        Color.yellow,
+    };
+    
+
     // Optional: assign color from inspector
-    public Color newColor = Color.red;
-
-    private SpriteRenderer spriteRenderer;
-
     void Awake()
     {
-        // Get the SpriteRenderer component
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        if (spriteRenderer != null)
-        {
-            // Change the color
-            spriteRenderer.color = newColor;
-        }
-        else
-        {
-            Debug.LogWarning("No SpriteRenderer found on this GameObject!");
-        }
+        Color randomColor = colors[Random.Range(0, colors.Count)];
+        spriteRenderer.color = randomColor;
     }
 }
