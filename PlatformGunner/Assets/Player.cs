9f381aaa0cd96db520cb00dd3ddf8b331e8020eb
tobiaslarsen1995 <tobiaslarsen1995@gmail.com>
2025-11-29 01:05:26 +0100

    Make GameObject Red

--- PlatformGunner/Assets/Player.cs
+++ PlatformGunner/Assets/Player.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class SpriteColorChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //New Change
-        //Change two
-        // Comment for new Branch
-    }
+    // Optional: assign color from inspector
+    public Color newColor = Color.red;
 
-    // Update is called once per frame
-    void Update()
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
     {
-        
+        // Get the SpriteRenderer component
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            // Change the color
+            spriteRenderer.color = newColor;
+        }
+        else
+        {
+            Debug.LogWarning("No SpriteRenderer found on this GameObject!");
+        }
     }
 }
